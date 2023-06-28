8523af9f902c4f89e71d488581af53e1f9df623e
Radouane0 <ikramaboufera@gmail.com>
2023-06-28 15:16:55 +0200

    Finalisation du projet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { useState } from "react"
+
 function App() {
+
+  const [ counter, setCounter ] = useState(0)
+
+  const increase = () => {
+    setCounter(count => count + 1)
+  }
+
+  const decrease = () => {
+    if(counter > 0) {
+      setCounter(count => count - 1)
+    }
+  }
+
+  const reset = () => {
+    setCounter(0)
+  }
+
+
+
   return (
-    <div className="container">
-      Hello World !!!
+    <div className="counter">
+      <h1>Voici un compteur</h1>
+      <span className="counter__output">{counter}</span>
+      <div className="btn-container">
+        <button className="control__btn" onClick={increase}>+</button>
+        <button className="control__btn" onClick={decrease}>-</button>
+        <button className="reset" onClick={reset}>Reset</button>
+      </div>
     </div>
   )
 }
